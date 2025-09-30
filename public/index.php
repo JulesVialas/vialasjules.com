14f98107967901b06f039740eac01dec458b98a7
Jules Vialas <jules.vialas@gmail.com>
2025-09-30 08:17:54 +0200

    test

--- public/index.php
+++ public/index.php
@@ -20,14 +20,12 @@ require_once __DIR__ . '/../src/Autoloader.php';
 $autoloader = new Autoloader();
 
 $autoloader->addNamespace('App', __DIR__ . '/../src');
-$autoloader->addNamespace('Celcat', __DIR__ . '/../src/Services');
 $autoloader->register();
 
 use App\Services\Language;
 use App\Services\Router;
 use App\Controllers\HomeController;
 use App\Controllers\ContactController;
-use App\Controllers\EmploiDuTempsControleur;
 
 try {
     // Initialize language service
@@ -37,7 +35,6 @@ try {
     $router = new Router();
     $router->get('/', [new HomeController(), 'index']);
     $router->post('/contact', [new ContactController(), 'submitForm']);
-    $router->get('/convert', [new EmploiDuTempsControleur(), 'convert']);
     
     // Parse request
     $uri = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? '', '/');
